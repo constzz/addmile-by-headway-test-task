64182e447c53f88f4f6113ba0fd8a8eb362a0a81
Konstantin Bezzemelnyi <>
2024-08-03 00:58:30 +0300

    Setup project

---
+++ BookListener/BookListener/BookListenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookListenerApp.swift
+//  BookListener
+//
+//  Created by Konstantin Bezzemelnyi on 02.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookListenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
