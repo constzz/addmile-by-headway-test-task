3277bf21287fd7426718e59165cced3b63fab211
Konstantin Bezzemelnyi <77053754+constzz@users.noreply.github.com>
2024-08-04 09:30:26 +0300

    Create ToggleView component and test with snapshots

--- BookListener/BookListenerTests/BookListenerSnapshotTests.swift
+++ BookListener/BookListenerTests/BookListenerSnapshotTests.swift
@@ -25,4 +25,18 @@ class BookListenerSnapshotTests: XCTestCase {
         let sut = SliderView(value: .constant(0), leftLabelValue: .constant("0"), rightLabelValue: .constant("100"))
         assertImageSnapshot(ofView: sut, name: .snapshotName400_per_200, config: .config_400_200_pixels)
     }
+    
+    func test_toggleView_listenMode() {
+        let view = ToggleView(
+            listenScreenMode: .constant(.listen), isAnimating: .constant(false)
+        )
+        self.assertImageSnapshot(ofView: view, name: .snapshotName400_per_200, config: .config_400_200_pixels)
+    }
+    
+    func test_toggleView_readMode() {
+        let view = ToggleView(
+            listenScreenMode: .constant(.read), isAnimating: .constant(false)
+        )
+        self.assertImageSnapshot(ofView: view, name: .snapshotName400_per_200, config: .config_400_200_pixels)
+    }
 }
